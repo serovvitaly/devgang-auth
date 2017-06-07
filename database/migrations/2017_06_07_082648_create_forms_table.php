78937b4bd2a0c8d10441ed526bc7b15ae4420a17
Vitaly Serov <vi.serov@teamber.ru>
2017-06-07 12:25:29 +0300

    Добавлены миграции для моделей Form и Domain

---
+++ database/migrations/2017_06_07_082648_create_forms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('domain_id');
+            $table->string('name', 80);
+            $table->string('title', 200);
+            $table->string('description', 600);
+            $table->timestamps();
+            $table->unique(['domain_id', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
---
+++ database/migrations/2017_06_07_083019_create_domains_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDomainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uid', 80)->unique();
+            $table->string('host', 180);
+            $table->integer('owner_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domains');
+    }
+}
