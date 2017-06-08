4959cbf633752b326af67848603b51dd7ceda578
Vitaly Serov <vi.serov@teamber.ru>
2017-06-08 20:09:54 +0300

    Добавлена консольная команда для установки приложения

---
+++ app/Console/Commands/Install.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Install extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Installation this application';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        \Artisan::call('migrate:refresh', [
+            '--force' => true,
+        ]);
+
+        \Artisan::call('db:seed', [
+            '--class' => 'DomainsAndFormsTableSeeder',
+        ]);
+    }
+}
